6ae9979232d46c88cdf98d836e8a903f78506d87
gjj <xyclr@163.com>
2023-08-15 14:58:50 +0800

    init

---
+++ packages/yarn-module1/index.js
@@ -0,0 +1,6 @@
+// yarn-module1/index.js
+function yarnModule1() {
+    console.log('yarn-module1');
+}
+
+module.exports = yarnModule1;
---
+++ packages/yarn-module2/index.js
@@ -0,0 +1,12 @@
+
+// yarn-module2/index.js
+const yarnModule1 = require('yarn-module1');
+
+function yarnModule2() {
+    console.log('yarn-module2');
+}
+
+yarnModule1();
+yarnModule2();
+
+module.exports = yarnModule2;
